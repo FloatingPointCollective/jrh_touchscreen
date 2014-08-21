fe9fd9f262cd9a31a48aeb53d47d4d2f1ca703db
Jack Kalish <jack@jackkalish.com>
2014-08-20 23:19:13 -0700

    added video player, added dynamic footer menu switching, css tweaks

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,7 +1,16 @@
 
 function uiCtrl($scope) {
 
-  $scope.hideMouse = false;
+  $scope.hideMouse = true;
+  $scope.videoPlaying = false;
+  $scope.activeVideoName = "";
+  $scope.footerMenuIsHidden = false;
+
+  $scope.footerItems = [
+    {label:'About the ILWU', text:''},
+    {label:'SF Port Commission', text:''},
+    {label:'Credits', text:''},
+    ];
 
     $scope.onKeyUp = function ($event) {
         if($event.keyCode == 32){
@@ -10,5 +19,39 @@ function uiCtrl($scope) {
         console.log("keypressed: " + $event.keyCode);
         console.log("$scope.hideMouse: "+$scope.hideMouse);
     };
+
+    $scope.playVideo = function(videoName){
+      $scope.videoPlaying = true;
+      $scope.activeVideoName = videoName;
+      console.log("play video!");
+
+      var myVideo = document.getElementById("videoPlayer"); 
+      myVideo.play();
+
+      myVideo.addEventListener('ended',$scope.onVideoDone,false);
+
+    }
+
+    $scope.onVideoDone = function(e){
+      console.log("video finished playing");
+      $scope.videoPlaying = false;
+      $scope.$apply();
+    }
+
+    $scope.selectFooterMenu = function(index){
+      $scope.hideFooterMenu();
+    }
+
+    $scope.hideFooterMenu = function(){
+      $scope.footerMenuIsHidden = true;
+    }
+
+    $scope.showFooterMenu = function(){
+      $scope.footerMenuIsHidden = false;
+    }
+
+    $scope.goHome = function(){
+      $scope.showFooterMenu();
+    }
 
 }
