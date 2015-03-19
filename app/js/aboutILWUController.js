4281beab97e0fa85af93643208bdb01387dd56fd
Jack Kalish <jack@jackkalish.com>
2015-03-19 14:24:35 -0700

    using partials for all about ill content

--- app/js/aboutILWUController.js
+++ app/js/aboutILWUController.js
@@ -24,21 +24,21 @@ angular
         {   
             id: 1,
             title:'Union Democracy', 
-            description:'A firm commitment to union democracy and membership participation has been the hallmark of the International Longshore and Warehouse Union since its beginning in the 1930s.  This is nearly unique in U.S. economic and industrial life.  In the ILWU, any member can speak at a union meeting and potentially influence overall ILWU policy. Explaining the union’s strength and its ability to command a dedicated following, Harry Bridges, the ILWU’s founding International President, said, "It was about democracy. We said that the rank and file had the right to decide, and if you gave them the facts, they’d make the right decision."', 
+            description:'partial', 
             imageID:'uniondemocracy', 
             caption:'ILWU Longshore workers voting in a union meeting (ILWU Archives)'
         },
         {   
             id: 2,
             title:'Who the ILWU Represents', 
-            description:'The International Longshore and Warehouse Union (ILWU) was founded following a major 1934 West Coast waterfront strike and is headquartered in San Francisco.  The original core group of workers loaded and unloaded ships’ cargo by hand using strength, skill, and ingenuity. The term "longshoreman" originated in the early days. When a ship approached shore, the captain, seeking extra hands to work cargo, beckoned to those in need of jobs, yelling "men along the shore." Today, the ILWU represents over 40,000 workers who belong to 60 local unions and divisions located along the U.S. West Coast, plus Hawaii, Panama and Canada. Almost 20,000 dockworkers continue to work on the waterfront at twenty-nine West Coast ports. Other ILWU members are employed in a wide variety of other industries, including hotels and tourism, industrial and warehousing, mining, agriculture, maritime, and retail.', 
+            description:'partial', 
             imageID:'representation', 
             caption:'A longshore worker on the docks in Southern California (ILWU Archives)'
         },
         {
             id: 3,
             title:'Ten Guiding Principles', 
-            description:'The Ten Guiding Principles of the ILWU. I. A Union is built on its members. II. Labor unity is at all times the key for a successful economic advancement. III. Workers are indivisible. IV. “To help any worker in distress” must be a daily guide in the life of every trade union and its individual members. V. Any union, if it is to fulfill its appointed task, must put aside all internal differences and issues to combine for the common cause of advancing the welfare of the membership. VI. The days are long gone when a union can consider dealing with single employers. VII. Just as water flows to its lowest level, so do wages if the bulk of the workers are left unorganized. VIII. The basic aspiration and desires of the workers throughout the world are the same. IX. A new type of unionism is called for which does not confine its ambitions and demands only to wages X. Jurisdictional warfare and jurisdictional raiding must be outlawed by labor itself.', 
+            description:'partial', 
             imageID:'principles', 
             caption:''
         }
